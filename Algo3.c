ff7ef75c0d8130f3eb8ae651b5e1d82a840e9b75
Hector Gonzalez <raftb.gonzalez@outlook.com>
2019-01-10 23:53:54 +0000

    Tic Tac Toe BoardGame

--- Algo3.c
+++ Algo3.c
@@ -15,21 +15,23 @@ void DrawBoard()
 {
 
     int i;
-    int j;
-    int board = 0;
+    char boardGame[9];
 
-    for(i = 0; i < 5; i++)
+
+    for(i = 0; i < 9; i++)
     {
-        board[i] = ' ';
-        printf("----+----\n");
-        for(j = 0; j < 6; j++)
-        {
-            board[j] = ' ';
-            printf("----+----\n");
-        }
+       boardGame[i] = ' ';
+
+    }
+       printf(" %c | %c | %c \n", boardGame[0], boardGame[1], boardGame[2]);
+       printf("----+----\n");
+       printf(" %c | %c | %c \n", boardGame[3], boardGame[4], boardGame[5]);
+       printf("----+----\n");
+       printf(" %c | %c | %c \n", boardGame[6], boardGame[7], boardGame[8]);
+
     }
 
 
-}
+
 
 
