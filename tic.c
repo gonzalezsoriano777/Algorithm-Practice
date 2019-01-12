11f661a24ed41d9a2405b65602946c9d5b47dcf8
Hector Gonzalez <raftb.gonzalez@outlook.com>
2019-01-12 17:24:17 +0000

    Test

---
+++ tic.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+
+char drawBoard();
+
+
+int main(void)
+{
+    printf("Hello, World\n");
+}
+
+char drawBoard(void)
+{
+    int x;
+    int o;
+
+    for (x = 0; x < 6; x++)
+    {
+        for(o = 0; o < 3; o++)
+        {
+
+        }
+
+    }
+}
