810e2d5fafe8e0b7b587a12cb9a0c6e46f4bea71
Hector Gonzalez <raftb.gonzalez@outlook.com>
2019-01-14 18:57:46 +0000

    Runs a tic tac toe game with 1 loop that iterates through a if statement then executes the game

--- tic.c
+++ tic.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include <cs50.h>
 
-char drawBoard();
+// This is the remake of the game, where we actually play a tic tac toe game
+void drawBoard();
 
 
 int main(void)
 {
-    printf("Hello, World\n");
+    drawBoard();
+    return 0;
 }
 
-char drawBoard(void)
+void drawBoard()
 {
-    int x;
-    int o;
+    char board[9];
+    int i;
 
-    for (x = 0; x < 6; x++)
+    for (i = 0; i < 9; i++)
     {
-        for(o = 0; o < 3; o++)
+        if(i % 2 == 1)
         {
-
+            board[i] = 'X';
+        } else
+        {
+            board[i] = 'O';
         }
+    }
+          printf(" %c | %c | %c \n", board[0], board[1], board[2]);
+          printf("----+----\n");
+          printf(" %c | %c | %c \n", board[3], board[4], board[5]);
+          printf("----+----\n");
+          printf(" %c | %c | %c \n", board[6], board[7], board[8]);
+          printf("Well guess O always marks the spot!\n");
 
     }
-}
