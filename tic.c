4b8d97aadf6998399eba594ee5085fc8579ed501
Hector Gonzalez <raftb.gonzalez@outlook.com>
2019-01-30 02:34:41 +0000

    test

--- tic.c
+++ tic.c
@@ -4,12 +4,15 @@
 // This is the remake of the game, where we actually play a tic tac toe game
 void drawBoard();
 
+
+// New array that will be put into the old array
 char boardGame[9] = {'1', '2', '3', '4', '5', '6', '7', '8'};
 
 void importBoard(char string[]);
 
 int main(void)
 {
+
     drawBoard();
     return 0;
 }
@@ -41,19 +44,20 @@ void drawBoard()
     printf(" +--+--+--+\n");
     printf(" %c | %c | %c\n", board[3], board[4], board[5]);
     printf(" +--+--+--+\n");
-     printf(" %c | %c | %c\n", board[6], board[7], board[8]);
+    printf(" %c | %c | %c\n", board[6], board[7], board[8]);
 
     printf("Well X always marks the spot!\n");
     printf("X won with %i\n", xCounter);
     printf("While O had %i\n", oCounter);
 }
 
-void importBoard(char string[])
+void importBoard(char string[3])
 {
-    int j;
 
-    for (j = 0; j < 9; j++)
+    int i;
+
+    for (i = 0; i < 9; i++)
     {
-
+        string[i] = "";
     }
 }
