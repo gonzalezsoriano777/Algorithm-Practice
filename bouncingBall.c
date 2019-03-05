c29890a1d0a184ed26a2941b7efc30db21872003
Hector Gonzalez <raftb.gonzalez@outlook.com>
2019-03-05 02:32:22 +0000

    Format shown

--- bouncingBall.c
+++ bouncingBall.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <cs50.h>
 
+// As shown from the format
+    /*
+
+    Beginning the order from the height of the floor,
+    The Rate of the ball bouncing from the height of the floor the child is on
+    Mother looking out of the window from below from the ball hitting the ground
+
+    */
+
 int Ballbouncing(double, double, double);
 
 int main(void)
 {
-    int Timin
+
+
+    int TimingofBall = Ballbouncing()
+}
+
+int Ballbouncing(double h, double br, double win)
+{
+
 }
