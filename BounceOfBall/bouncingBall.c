df215dcab7515bd7721e796c7ad4b14ab6a4cb3a
Hector Gonzalez <raftb.gonzalez@outlook.com>
2019-03-07 15:25:05 +0000

    Edit, redone it to see if there was an easier process

--- BounceOfBall/bouncingBall.c
+++ BounceOfBall/bouncingBall.c
@@ -1,38 +1,37 @@
 #include <stdio.h>
-#include <cs50.h>
+#include <stdlib.h>
+
 
 // As shown from the format
     /*
-
     Beginning the order from the height of the floor,
     The Rate of the ball bouncing from the height of the floor the child is on
     Mother looking out of the window from below from the ball hitting the ground
-
     */
 
-int Ballbouncing(double, double, double);
+
+int BouncingBall(double, double, double);
+
 
 int main(void)
 {
-    int TimeofBallBouncing = Ballbouncing(3.0, .66, 1.5);
-    // printf("Hello, World\n");
-    printf("The # of Times of the ball was viewed as %i times!\n", TimeofBallBouncing);
+    int TimingOfBall = BouncingBall(3.0, .66, 1.5);
+    printf("The # of times of the ball was viewed as %i times!\n", TimingOfBall);
 
 }
 
-int Ballbouncing(double h, double br, double win) // h - height, br - rate of ball bouncing, win - window
+int BouncingBall(double h, double br, double win) // h - height, br - bounce rate of ball, win - window
 {
-    // key idea here is, if the condition is not correct
-    if (h <= 0 || (br <= 0 || br >= 1) || win >= h)
+    // key idea is, that if the condition is not met then
+    if(h <= 0 || (br <= 0 || br >= 1) || win >= h)
     {
-        return -1; // then return an errors that will break the function
-
+        return -1; // return an error and break function
     }
     else
     {
         float Rebound = 0;
 
-        // ensures, height of 'rebouncing' ball is greater then win param
+        // ensures that the "rebouncing" ball is greater then the win param
         Rebound = h * br;
 
         int viewCount = 1;
@@ -40,22 +39,21 @@ int Ballbouncing(double h, double br, double win) // h - height, br - rate of ba
         while(Rebound > win)
         {
             Rebound = Rebound * br;
-
             viewCount = viewCount + 2;
-
         }
 
-        return viewCount; // Number of time the ball is viewed
+        return viewCount;
+
     }
+
+
 }
+
 
 // Condition
 
     /*
-
     ** Float parameter "h" in meters must be greater than 0**
     ** Float parameter “br" must be greater than 0 and less than 1**
     ** Float parameter “win" must be less than h.**
-
     */
-
