2faab9aabde6a2602fe386b90b9cfd5bc30e0e43
Hector Gonzalez <raftb.gonzalez@outlook.com>
2019-03-09 16:56:37 +0000

    test

--- TicTacToe/newDrawbord.c
+++ TicTacToe/newDrawbord.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// New drawboard using only the formula, W X H // Width and Height
+
+void drawboard(int, int);
+
+
 int main(void)
 {
     printf("Hello, World\n");
+
+    // Width and Height of the drawboard
+    drawboard(6, 4);
 }
+
+void drawboard(int x, int y)
+{
+
+}
+
 
