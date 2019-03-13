5ba8d5e309ad588b7e53fca5153f89bac9aa7bb6
Hector Gonzalez <raftb.gonzalez@outlook.com>
2019-03-13 01:08:27 +0000

    test

---
+++ TicTacToe/Drawboardv2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// This is the 2nd Version of the drawboard, only include the height and the width of the board
+int drawboard(int, int);
+
+int main(void)
+{
+    printf("Hello, World\n");
+
+
+    drawboard(6, 4);
+}
+
+int drawboard(int x, int y)
+{
+    int size = 1;
+
+    for(int i = 0; i < 0; i++)
+    {
+
+    }
+
+    return size;
+}
